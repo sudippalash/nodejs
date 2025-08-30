705e7c99fea53e6c44501746dde4047986f647c0
Sudip Palash <palash.sudip@gmail.com>
2025-08-31 02:56:03 +0600

    add UserController and modify route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
+import UserController from "../controllers/UserController";
 
 const router = Router();
 
-// Define a route
+// Default route for /api
 router.get('/', (req, res) => {
     res.send('this is api route');
 });
 
-router.get('/users', (req, res) => {
-    res.send('this is users route');
-});
+router.get("/users", UserController.index);
+router.get("/users/:id", UserController.show);
+router.post("/users/", UserController.create);
+router.put("/users/:id", UserController.update);
+router.delete("/users/:id", UserController.destroy);
 
 export default router;
