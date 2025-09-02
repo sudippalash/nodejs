6365d5466b74b822895ac48b2055233db9666459
Sudip Palash <palash.sudip@gmail.com>
2025-09-03 04:03:29 +0600

    test mail example code removed

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,14 +1,12 @@
 import express, { Request, Response } from "express";
 import { createResourceRouter } from "../helpers/ResourceRouter";
 import UserController from "../controllers/UserController";
-import TestMailController from "../controllers/TestMailController";
 
 const app = express();
 
 const router = createResourceRouter();
 
 // Default route for /api
-router.get('/mail', TestMailController.index);
 router.get('/', (req: Request, res: Response) => {
     res.json({ message: "API Working" });
 });
